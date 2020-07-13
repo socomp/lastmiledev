5799a4c34a7a6883bc99207088c7447b6e4b445a
Sam Smith <developer@lastmile.shop>
2020-07-13 06:05:38 +0000

    lastmile_theme base install

---
+++ themes/theme_lastmile/__manifest__.py
@@ -0,0 +1,10 @@
+{
+  'name':'Last Mile theme',
+  'description': 'A starting theme ex.',
+  'version':'0.1.0',
+  'author':'Samuel Smith',
+
+  'data': [ 'views/layout.xml' ],
+  'category': 'Theme/Creative',
+  'depends': ['website', 'website_theme_install', 'sale'],
+}
